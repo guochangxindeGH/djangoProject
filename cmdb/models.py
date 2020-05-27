c859ed0f52375610f69ac9230d680ebf7cb1b5be
Jon <291976422@qq.com>
2020-05-27 23:07:29 +0800

    # feat: 随便加一张表

--- cmdb/models.py
+++ cmdb/models.py
@@ -7,3 +7,7 @@ class UserInfo(models.Model):  # 需要继承Model类
     # 创建两个char类型字段，最大长度为32
     user = models.CharField(max_length=32)
     pwd = models.CharField(max_length=32)
+
+
+class Article(models.Model):
+    content = models.TextField(null=True)
